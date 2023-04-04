8026ca10eb5f7551995f6b49c2a8c59a4886111e
0xS3GF4VLT <0xs3gf4vlt@gmail.com>
2023-04-03 21:35:11 -0400

    initial commit

---
+++ wwwroot/js/jsx/views/Index.tsx
@@ -0,0 +1,12 @@
+﻿type AppProps = {
+    message: string;
+};
+
+const Page = ({ message }: AppProps) => (
+    <div class="text-center">
+        <h1 class="display-4">Welcome</h1>
+        <p>Learn about <a href="https://docs.microsoft.com/aspnet/core">building Web apps with ASP.NET Core</a>.</p>
+    </div>
+);
+
+ReactDOM.render(<Page />, document.getElementById('content'));
